ee5af381312276e32363e5a8e0bae6ff96ca9b54
dhruvipadsala9818 <dhruvipadsala9818@gmail.com>
2024-10-05 13:49:04 +0530

    bug fixes

--- lib/controller/bottom_controller.dart
+++ lib/controller/bottom_controller.dart
@@ -7,20 +7,23 @@ import 'package:aniwatch_tv/ui/main/settings_pages/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class BottomController extends GetxController{
-
-  Widget selectedWidget =  const HomeScreen(name:'');
-  Widget previousWidget = const HomeScreen(name: '',);
+class BottomController extends GetxController {
+  Widget selectedWidget = const HomeScreen(name: '');
+  Widget previousWidget = const HomeScreen(
+    name: '',
+  );
 
   List screenName = [
-    const HomeScreen(name: '',),
+    const HomeScreen(
+      name: '',
+    ),
     const ExploreScreen(),
     const SettingsScreen(),
   ];
 
   int selectedPage = 0;
+  // int selectedPage = 0;
   String userName = '';
-
 
   void setUserName(String name) {
     userName = name;
@@ -28,39 +31,41 @@ class BottomController extends GetxController{
     update();
   }
 
-
-  void updatePage(int pageIndex){
+  void updatePage(int pageIndex) {
     selectedPage = pageIndex;
     selectedWidget = screenName[pageIndex];
     update();
   }
 
-  void navigateToDetailsScreen(Map<String,dynamic> data,bool from){
+  void navigateToDetailsScreen(Map<String, dynamic> data, bool from) {
     previousWidget = selectedWidget;
-    selectedWidget = AnimeDetailScreen(data: data, from: from,);
+    selectedWidget = AnimeDetailScreen(
+      data: data,
+      from: from,
+    );
     update();
   }
 
-  void navigateToSeeAll(){
+  void navigateToSeeAll() {
     selectedWidget = const SeeAllScreen();
     update();
   }
 
-  void getBack(bool from){
-
-    if(from){
+  void getBack(bool from) {
+    if (from) {
       selectedWidget = previousWidget;
-    }else{
-      selectedWidget =  screenName[selectedPage];
+    } else {
+      selectedWidget = screenName[selectedPage];
     }
     update();
   }
 
-  void navigateToVideoScreen(String id,String thumbnail){
-    previousWidget= selectedWidget;
-        selectedWidget = VideoScreen(id: id,thumbnail: thumbnail,);
+  void navigateToVideoScreen(String id, String thumbnail) {
+    previousWidget = selectedWidget;
+    selectedWidget = VideoScreen(
+      id: id,
+      thumbnail: thumbnail,
+    );
     update();
   }
-
-
 }
